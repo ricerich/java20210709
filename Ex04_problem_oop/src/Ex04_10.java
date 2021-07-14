e80c4dd63a107e7b84d29fe7b60d83abe03fecdd
admin <admin@DESKTOP-3PE3QU4>
2021-07-14 14:30:43 +0900

    11 번까지

--- Ex04_problem_oop/src/Ex04_10.java
+++ Ex04_problem_oop/src/Ex04_10.java
@@ -1,8 +1,9 @@
 
 public class Ex04_10 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		new DicApp().run();
 
 	}
 
