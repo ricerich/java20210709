db14869d289bf46b5f04199770d6e02de71a03c0
admin <admin@DESKTOP-3PE3QU4>
2021-07-14 10:43:28 +0900

    8,9번

--- Ex04_problem_oop/src/Ex04_08.java
+++ Ex04_problem_oop/src/Ex04_08.java
@@ -1,8 +1,10 @@
 
 public class Ex04_08 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		PhoneBook pb = new PhoneBook();
+		pb.run();
 
 	}
 
--- Ex04_problem_oop/src/Ex04_09.java
+++ Ex04_problem_oop/src/Ex04_09.java
@@ -1,8 +1,14 @@
 
 public class Ex04_09 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int arr1[] = {1,5,7,9};
+		int arr2[] = {3,6,-1,100,77};
+		
+		int arr3[] = ArrayUtil.concat(arr1, arr2); 
+		ArrayUtil.print(arr3);
+		
 
 	}
 
