5fbc167c61aaaf6557759a144ca886afc9401358
admin <admin@DESKTOP-3PE3QU4>
2021-07-16 13:44:08 +0900

    5장 예제 5번까지

---
+++ Ch05_Inherit/src/Ex05_10.java
@@ -0,0 +1,9 @@
+
+public class Ex05_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ch05_Inherit/src/Shape.java
@@ -0,0 +1,10 @@
+
+class Shape 
+{ // ½´ÆÛ Å¬·¡½º
+	public Shape next; 
+	public Shape() { next = null; }
+
+	public void draw() {
+		System.out.println("Shape");
+	}
+}
