1f2c056d74be4b96f7ff19c93e56745bf1a8b53e
admin <admin@DESKTOP-3PE3QU4>
2021-07-19 11:35:02 +0900

    5장 8번9번

--- Ch05_Inherit/src/PhoneInterface.java
+++ Ch05_Inherit/src/PhoneInterface.java
@@ -1,9 +1,9 @@
 
 public interface PhoneInterface 
 {
-	final int TIMEOUT = 10000; // 상수 필드 선언
+	static final int TIMEOUT = 10000; // 상수 필드 선언
 	
-	void sendCall(); // 추상 메소드
+	abstract void sendCall(); // 추상 메소드
 	void receiveCall(); // 추상 메소드
 	
 	default void printLogo() 
