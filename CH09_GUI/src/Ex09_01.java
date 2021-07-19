713a6768768dc24879f75b302a909d3206395f26
admin <admin@DESKTOP-3PE3QU4>
2021-07-19 12:44:09 +0900

    9장 1번

---
+++ CH09_GUI/src/Ex09_01.java
@@ -0,0 +1,11 @@
+
+public class Ex09_01 {
+
+	public static void main(String[] args) 
+	{
+//		MyFrame mf = new MyFrame();
+		new MyFrame();
+
+	}
+
+}
---
+++ CH09_GUI/src/MyFrame.java
@@ -0,0 +1,15 @@
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame
+{
+
+	public MyFrame()  
+	{
+		setTitle("첫번째 프레임");
+		setSize(300,500);
+		setVisible(true);
+	}
+	
+}
